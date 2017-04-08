1b6fc96eada0f2abe31ff2808efdff8e21175e16
mvitsor <mvitsor@gmail.com>
2017-04-07 23:37:41 -0300

    primeira versão do código de sumô

--- C/Tarefa/main.c
+++ C/Tarefa/main.c
@@ -5,6 +5,10 @@
  * 03/2017
  */
 
+/*
+ * Vitor Sternlicht 
+ */
+
 #include <avr/io.h>
 #include <avr/wdt.h>
 #include <avr/interrupt.h>
@@ -13,6 +17,13 @@
 #include "sensors.h"
 #include "motors.h"
 
+// Tweak constants for maximum performance
+#define thresF 500
+#define thresL 400
+#define attack_speed 255
+#define turning_speed 200
+
+
 int main() {
 	sensors_init();
 	motors_init();
@@ -20,10 +31,22 @@ int main() {
 	wdt_reset();
 	wdt_disable();
 
-	for (;;) {
-		motors(200,200);
-		get_sensor(SENSOR_FD);
+	uint16_t FE = 0, FD = 0, LE = 0, LD = 0;	// these variables will store the values that the sensors read
+	int8_t dir = 1; 							// this is the turning direction
+
+	while (1) {
 		
+		// read sensor values
+		FE = get_sensor(SENSOR_FE), FD = get_sensor(SENSOR_FD), LE = get_sensor(SENSOR_LE), LD = get_sensor(SENSOR_LD);
+		
+		if (FE < thresF && FD < thresF) {					// if both front sensors see something
+			motors(attack_speed, attack_speed);				// move forward with attack speed
+		} else {											// if they don't, then turn
+			if (FE < thresL || LE < thresL) dir = 1;		// right, if the last sensor to see something is in the left 
+			else if (FD < thresL || LD < thresL) dir = -1;	// left, if the last sensor to see something is in the right
+			motors(dir*turning_speed, -dir*turning_speed);	// (here's the actual turning)
+		}
+
 		_delay_ms(10);
 	}
 
